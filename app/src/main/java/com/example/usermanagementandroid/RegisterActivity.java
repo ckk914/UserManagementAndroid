ae560f751c1db909bd9fd3216aeca62f6b5e21e4
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2025-08-01 00:52:50 +0900

    fix: 액티비티 <> 레이아웃 매핑 수정      각 컨트롤 변수로 받음

--- app/src/main/java/com/example/usermanagementandroid/RegisterActivity.java
+++ app/src/main/java/com/example/usermanagementandroid/RegisterActivity.java
@@ -1,6 +1,8 @@
 package com.example.usermanagementandroid;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +17,19 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register); //회원 등록 화면으로 view 세팅 🌟액티비티와 레이아웃을 연결해준다~!
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
+
+        //회원 등록에 관한 화면 처리에 대한 액티비티가 구성이 되어야함
+        EditText idText       = (EditText) findViewById(R.id.idText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        EditText nameText     = (EditText) findViewById(R.id.nameText);
+        EditText ageText      = (EditText) findViewById(R.id.ageText);
+
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+    } //end onCreate
 }
