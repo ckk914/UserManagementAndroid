d02745a3bb45449ae152c43e6e62b3601c85da38
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2025-08-01 01:14:14 +0900

    add: 액티비티 사이의 화면 전환 & 디자인과 액티비티 사이의 변수 설정

--- app/src/main/java/com/example/usermanagementandroid/LoginActivity.java
+++ app/src/main/java/com/example/usermanagementandroid/LoginActivity.java
@@ -1,6 +1,11 @@
 package com.example.usermanagementandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,5 +26,21 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
+
+        EditText idText       = (EditText) findViewById(R.id.idText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        TextView registerButton = (TextView) findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //로그인 화면에서 회원가입 화면으로 이동하도록 설정
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
+    } //end onCreate
+
 }
--- app/src/main/java/com/example/usermanagementandroid/MainActivity.java
+++ app/src/main/java/com/example/usermanagementandroid/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.usermanagementandroid;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,11 +17,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
-    }
+
+        EditText idText       = (EditText) findViewById(R.id.idText);
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+        TextView welcomeMessage = (TextView) findViewById(R.id.welcomeMessage);
+    } //end onCreate
 }
