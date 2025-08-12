c5781c4789b2e865f489eedc4fe14ec68064574e
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2025-08-13 01:04:29 +0900

    add: admin 관리자 - 회원 목록 리스트 볼 수 있도록 화면 추가

---
+++ app/src/main/java/com/example/usermanagementandroid/ManagementActivity.java
@@ -0,0 +1,24 @@
+package com.example.usermanagementandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+//로그인 성공하면 나오는 화면
+public class ManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_management);
+
+    }
+}
