19945e28603fc52e49b827203f254a9197150d7f
KyoungkonCho <50573460+ckk914@users.noreply.github.com>
2025-08-17 18:23:47 +0900

    edit: 회원 관리 페이지 테스트 data-> 실 data 넣도록 수정

--- app/src/main/java/com/example/usermanagementandroid/ManagementActivity.java
+++ app/src/main/java/com/example/usermanagementandroid/ManagementActivity.java
@@ -16,6 +16,9 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.usermanagementandroid.Adapter.UserListAdapter;
 import com.example.usermanagementandroid.Entity.User;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,15 +39,35 @@ public class ManagementActivity extends AppCompatActivity {
         //init
         listView = (ListView) findViewById(R.id.userListView);
         userList = new ArrayList<>();
-
-        userList.add(new User("id1","pw1","name1","age1"));
-        userList.add(new User("id2","pw2","name2","age2"));
-        userList.add(new User("id3","pw3","name3","age3"));
+        //테스트용 데이터 주석
+        //        userList.add(new User("id1","pw1","name1","age1"));
+        //        userList.add(new User("id2","pw2","name2","age2"));
+        //        userList.add(new User("id3","pw3","name3","age3"));
         listView = findViewById(R.id.userListView);
 
         userAdapter = new UserListAdapter(getApplicationContext(),userList);
         listView.setAdapter(userAdapter); //리스트뷰에 어댑터 세팅함.
 
+        //DB user를 보기 위한 try
+        try{
+            JSONObject jsonObject = new JSONObject(intent.getStringExtra("userList"));
+            JSONArray jsonArray = jsonObject.getJSONArray("response"); //받아올 배열 이름
+            int count = 0;
+            String userID, userPassword, userName, userAge;
+            while(count < jsonArray.length()){
+                JSONObject object = jsonArray.getJSONObject(count);
+                userID = object.getString("userID");
+                userPassword = object.getString("userPassword");
+                userName = object.getString("userName");
+                userAge = object.getString("userAge");
+                User user = new User(userID,userPassword,userName,userAge);
+                userList.add(user);
+                count++;
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
